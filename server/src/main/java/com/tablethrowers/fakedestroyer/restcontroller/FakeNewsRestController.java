eadff133c363a9644fc59cfe46c47527ac66b35e
Michał Pocheć (michpoc547) <michpoc547@student.polsl.pl>
2019-05-31 21:40:23 +0200

    Dodane api do oceniania stronek (serwer)

--- server/src/main/java/com/tablethrowers/fakedestroyer/restcontroller/FakeNewsRestController.java
+++ server/src/main/java/com/tablethrowers/fakedestroyer/restcontroller/FakeNewsRestController.java
@@ -3,10 +3,7 @@ package com.tablethrowers.fakedestroyer.restcontroller;
 import com.tablethrowers.fakedestroyer.entity.WebPage;
 import com.tablethrowers.fakedestroyer.util.IDataAccessObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,12 +14,37 @@ public class FakeNewsRestController {
     @Autowired
     private IDataAccessObject dataAccessObject;
 
+
+    /*
+     * Pobieranie danych o stronie po urlu
+     *
+     */
     @GetMapping("/webpage/{url}")
-    public WebPage getWebPageMarks(@PathVariable String url) { //metoda do pobierania fake i notfake z serwera
+    public WebPage getWebPageData(@PathVariable String url) { //pobieranie danych strony po urlu
 
         List<WebPage> resultList = dataAccessObject.getByStringValue(WebPage.class, "page_url", url);
 
         return resultList.get(0);
+    }
+
+
+    /*
+     * Ocenianie strony
+     * value == 0 -> ocena fake, value != 0 -> ocena notFake
+     *
+     */
+    @PutMapping("/webpage/{url}/{value}")
+    public void markWebPage(@PathVariable String url, @PathVariable int value){
+
+        WebPage webPage = getWebPageData(url);
+
+        if (value == 0) {
+            webPage.setFake(webPage.getFake() + 1);
+        } else {
+            webPage.setNotFake(webPage.getNotFake() + 1);
+        }
+
+        dataAccessObject.save(webPage);
     }
 
 }
