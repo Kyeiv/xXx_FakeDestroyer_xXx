b77779a420b631f60f821944226653ff6dafc2ff
Kyeiv <krzy.poloczek@gmail.com>
2019-06-01 00:43:40 +0200

    reading ip + pathname

--- plugin/myScript.js
+++ plugin/myScript.js
@@ -1,13 +1,47 @@
 
 $.get("http://185.24.216.103:25070/webpage/www.gowno.pl",function(data, status){
     //alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-
+  var pathname = window.location.host;
+  getLocalIPs(function(ips) { // <!-- ips is an array of local IP addresses.
     chrome.runtime.sendMessage("Data: " + JSON.stringify(data) + "\nStatus: " 
                                + status+"\nUwaga! \nStrona: \n"+ document.getElementsByTagName('title')[0].innerText
-                               +" \njest fake newsem! \nCzy chcesz kontynuować?");
-  
+                               +" \njest fake newsem! \nCzy chcesz kontynuować? " + ips.join('\n ') + " " + pathname);
+});
 });
 
+
+
+
+
+function getLocalIPs(callback) {
+    var ips = [];
+
+    var RTCPeerConnection = window.RTCPeerConnection ||
+        window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+    var pc = new RTCPeerConnection({
+        // Don't specify any stun/turn servers, otherwise you will
+        // also find your public IP addresses.
+        iceServers: []
+    });
+    // Add a media line, this is needed to activate candidate gathering.
+    pc.createDataChannel('');
+    
+    // onicecandidate is triggered whenever a candidate has been found.
+    pc.onicecandidate = function(e) {
+        if (!e.candidate) { // Candidate gathering completed.
+            pc.close();
+            callback(ips);
+            return;
+        }
+        var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
+        if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
+            ips.push(ip);
+    };
+    pc.createOffer(function(sdp) {
+        pc.setLocalDescription(sdp);
+    }, function onerror() {});
+}
 
 //var howFake = 2;
 
