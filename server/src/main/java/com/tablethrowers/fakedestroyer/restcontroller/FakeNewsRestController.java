1e6a1ed9f1a8d0edc6e4c102b069c1235b4e957f
Michał Pocheć (michpoc547) <michpoc547@student.polsl.pl>
2019-05-31 23:56:01 +0200

    Dodane dodawanie i pobieranie komentarzy do danej stronki

--- server/src/main/java/com/tablethrowers/fakedestroyer/restcontroller/FakeNewsRestController.java
+++ server/src/main/java/com/tablethrowers/fakedestroyer/restcontroller/FakeNewsRestController.java
@@ -1,8 +1,11 @@
 package com.tablethrowers.fakedestroyer.restcontroller;
 
+import com.tablethrowers.fakedestroyer.entity.Comment;
 import com.tablethrowers.fakedestroyer.entity.WebPage;
 import com.tablethrowers.fakedestroyer.util.IDataAccessObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,17 +17,22 @@ public class FakeNewsRestController {
     @Autowired
     private IDataAccessObject dataAccessObject;
 
-
     /*
      * Pobieranie danych o stronie po urlu
      *
      */
     @GetMapping("/webpage/{url}")
-    public WebPage getWebPageData(@PathVariable String url) { //pobieranie danych strony po urlu
+    public ResponseEntity<WebPage> getWebPageData(@PathVariable String url) { //pobieranie danych strony po urlu
 
         List<WebPage> resultList = dataAccessObject.getByStringValue(WebPage.class, "page_url", url);
 
-        return resultList.get(0);
+        System.out.println("WESZŁO DO POBIERANIA DANYCH STRONY");
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        responseHeaders.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        return ResponseEntity.ok().headers(responseHeaders).body(resultList.get(0));
     }
 
 
@@ -36,7 +44,7 @@ public class FakeNewsRestController {
     @PutMapping("/webpage/{url}/{value}")
     public void markWebPage(@PathVariable String url, @PathVariable int value){
 
-        WebPage webPage = getWebPageData(url);
+        WebPage webPage = dataAccessObject.getByStringValue(WebPage.class, "page_url", url).get(0);
 
         if (value == 0) {
             webPage.setFake(webPage.getFake() + 1);
@@ -45,6 +53,47 @@ public class FakeNewsRestController {
         }
 
         dataAccessObject.save(webPage);
+
+        System.out.println("WESZŁO DO INKREMENTACJI FAKE/NOTFAKE");
     }
 
+
+    /*
+     * Wysyłanie komentarza do konkretnej strony
+     *
+     */
+    @PostMapping("/webpage/{url}/add-comment/{comment}")
+    public void leaveComment(@PathVariable String url, @PathVariable String comment){
+
+        WebPage webPage = dataAccessObject.getByStringValue(WebPage.class, "page_url", url).get(0);
+
+        Comment newComment = new Comment();
+
+        newComment.setId(0);
+        newComment.setWebPage(webPage);
+        newComment.setDescription(comment);
+        newComment.setDislikes(0);
+        newComment.setLikes(0);
+
+        dataAccessObject.save(newComment);
+
+        System.out.println("WESZŁO DO ZAPISU KOMENTARZA");
+    }
+
+
+    /*
+     * Pobieranie komentarzy dla wybranego urla
+     *
+     */
+    @GetMapping("/comment/{url}")
+    public ResponseEntity<List<Comment>> getCommentsForWebPage(@PathVariable String url){
+
+        WebPage webPage = dataAccessObject.getByStringValue(WebPage.class, "page_url", url).get(0);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        responseHeaders.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        return ResponseEntity.ok().headers(responseHeaders).body(webPage.getComments());
+    }
 }
