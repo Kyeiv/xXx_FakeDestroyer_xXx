4fc04a9645bfbbb66b936e378f30bb0153ea7f9b
Michał Pocheć (michpoc547) <michpoc547@student.polsl.pl>
2019-06-01 02:29:23 +0200

    Dodane pare rzeczy, nie wiem juz co, chce spac xd

---
+++ server/src/main/java/com/tablethrowers/fakedestroyer/entity/UserConnection.java
@@ -0,0 +1,73 @@
+package com.tablethrowers.fakedestroyer.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="users_connections")
+public class UserConnection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "time_stamp")
+    private long timestamp;
+
+    @Column(name = "ip")
+    private String ip;
+
+    @JsonIgnore
+    @JoinColumn(name = "id_webpage")
+    @ManyToOne
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    private WebPage webPage;
+
+    public UserConnection(long timestamp, String ip, WebPage webPage) {
+        this.timestamp = timestamp;
+        this.ip = ip;
+        this.webPage = webPage;
+    }
+
+    public UserConnection() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public WebPage getWebPage() {
+        return webPage;
+    }
+
+    public void setWebPage(WebPage webPage) {
+        if(Objects.equals(this.webPage, webPage))
+            return;
+        WebPage oldWebPage = this.webPage;
+        this.webPage = webPage;
+    }
+}
--- server/src/main/java/com/tablethrowers/fakedestroyer/restcontroller/FakeNewsRestController.java
+++ server/src/main/java/com/tablethrowers/fakedestroyer/restcontroller/FakeNewsRestController.java
@@ -1,6 +1,7 @@
 package com.tablethrowers.fakedestroyer.restcontroller;
 
 import com.tablethrowers.fakedestroyer.entity.Comment;
+import com.tablethrowers.fakedestroyer.entity.UserConnection;
 import com.tablethrowers.fakedestroyer.entity.WebPage;
 import com.tablethrowers.fakedestroyer.util.IDataAccessObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -18,33 +20,89 @@ public class FakeNewsRestController {
     private IDataAccessObject dataAccessObject;
 
     /*
-     * Pobieranie danych o stronie po urlu
+     * Pobieranie danych o stronie po urlu - jeśli taki nie istnieje zwraca obiekt wypelniony pustymi/zerowymi polami
      *
      */
     @GetMapping("/webpage/{url}")
-    public ResponseEntity<WebPage> getWebPageData(@PathVariable String url) { //pobieranie danych strony po urlu
+    public ResponseEntity<WebPage> getWebPageData(@PathVariable String url) {
+
+        System.out.println("WESZŁO DO POBIERANIA DANYCH STRONY");
 
         List<WebPage> resultList = dataAccessObject.getByStringValue(WebPage.class, "page_url", url);
 
-        System.out.println("WESZŁO DO POBIERANIA DANYCH STRONY");
+        WebPage webPage;
+
+        if(resultList.isEmpty()) {
+            webPage = new WebPage();
+            webPage.setPage_url("");
+            webPage.setName("");
+            webPage.setFake(0);
+            webPage.setNotFake(0);
+            webPage.setId(0);
+        } else {
+            webPage = resultList.get(0);
+        }
 
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("Access-Control-Allow-Origin", "*");
         responseHeaders.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-        return ResponseEntity.ok().headers(responseHeaders).body(resultList.get(0));
+        return ResponseEntity.ok().headers(responseHeaders).body(webPage);
     }
 
 
     /*
-     * Ocenianie strony
+     * Ocenianie strony. Jeśli takowa nie istnieje - dodaje ją do bazy i wstawia pierwszą ocenę
      * value == 0 -> ocena fake, value != 0 -> ocena notFake
      *
      */
-    @PutMapping("/webpage/{url}/{value}")
-    public void markWebPage(@PathVariable String url, @PathVariable int value){
+    @PostMapping("/webpage/{url}/mark/{value}")
+    public ResponseEntity<Boolean> markWebPage(@PathVariable String url, @PathVariable int value, @RequestParam String ip, @RequestParam String domain){
 
-        WebPage webPage = dataAccessObject.getByStringValue(WebPage.class, "page_url", url).get(0);
+        System.out.println("WESZŁO DO INKREMENTACJI FAKE/NOTFAKE");
+
+        List<UserConnection> userLogs = dataAccessObject.getByStringValue(UserConnection.class, "ip", ip);
+        UserConnection userConnection = null;
+
+        Boolean canMark = false;
+
+        if(!userLogs.isEmpty()) {
+            for (UserConnection uc : userLogs) {
+                if (uc.getWebPage().getPage_url().equals(url))
+                    if (uc.getTimestamp() + 86400000L > new Date().getTime()) {
+                        canMark = false;
+                        break;
+                    } else {
+                        userConnection = uc;
+                        canMark = true;
+                    }
+            }
+        } else {
+            canMark = true;
+        }
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        responseHeaders.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        if(!canMark)
+            return ResponseEntity.ok().headers(responseHeaders).body(canMark);
+
+
+        List<WebPage> webPages = dataAccessObject.getByStringValue(WebPage.class, "page_url", url);
+
+        WebPage webPage;
+
+        if(webPages.isEmpty()){
+            webPage = new WebPage();
+            webPage.setId(0);
+            webPage.setNotFake(0);
+            webPage.setFake(0);
+            webPage.setName("");
+            webPage.setPage_url(url);
+        } else {
+            webPage = webPages.get(0);
+        }
 
         if (value == 0) {
             webPage.setFake(webPage.getFake() + 1);
@@ -52,20 +110,44 @@ public class FakeNewsRestController {
             webPage.setNotFake(webPage.getNotFake() + 1);
         }
 
-        dataAccessObject.save(webPage);
+        if(userLogs.isEmpty()){
+            userConnection = new UserConnection();
+            userConnection.setId(0);
+        }
 
-        System.out.println("WESZŁO DO INKREMENTACJI FAKE/NOTFAKE");
+        userConnection.setIp(ip);
+        userConnection.setTimestamp(new Date().getTime());
+        userConnection.setWebPage(webPage);
+
+        dataAccessObject.save(userConnection);
+
+        return ResponseEntity.ok().headers(responseHeaders).body(canMark);
     }
 
 
     /*
-     * Wysyłanie komentarza do konkretnej strony
+     * Wysyłanie komentarza do konkretnej strony - jak nie istnieje to sie tworzy i dodaje pierwszy komentarz
      *
      */
     @PostMapping("/webpage/{url}/add-comment/{comment}")
     public void leaveComment(@PathVariable String url, @PathVariable String comment){
 
-        WebPage webPage = dataAccessObject.getByStringValue(WebPage.class, "page_url", url).get(0);
+        System.out.println("WESZŁO DO ZAPISU KOMENTARZA");
+
+        List<WebPage> webPages = dataAccessObject.getByStringValue(WebPage.class, "page_url", url);
+
+        WebPage webPage;
+
+        if(webPages.isEmpty()){
+            webPage = new WebPage();
+            webPage.setId(0);
+            webPage.setNotFake(0);
+            webPage.setFake(0);
+            webPage.setName("");
+            webPage.setPage_url(url);
+        } else {
+            webPage = webPages.get(0);
+        }
 
         Comment newComment = new Comment();
 
@@ -76,8 +158,6 @@ public class FakeNewsRestController {
         newComment.setLikes(0);
 
         dataAccessObject.save(newComment);
-
-        System.out.println("WESZŁO DO ZAPISU KOMENTARZA");
     }
 
 
@@ -94,6 +174,30 @@ public class FakeNewsRestController {
         responseHeaders.set("Access-Control-Allow-Origin", "*");
         responseHeaders.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
+        System.out.println("WESZŁO DO POBIERANIA KOMENTARZY DO URLA");
+
         return ResponseEntity.ok().headers(responseHeaders).body(webPage.getComments());
+    }
+
+
+    /*
+     * Ocenianie komentarza o podanym id
+     * value == 0 -> dislike, value != 0 -> like
+     *
+     */
+    @PostMapping("/comment/{id}/mark/{value}")
+    public void markComment(@PathVariable int id, @PathVariable int value){
+
+        Comment comment = dataAccessObject.getById(Comment.class, id);
+
+        if(value == 0){
+            comment.setDislikes(comment.getDislikes() + 1);
+        } else {
+            comment.setLikes(comment.getLikes() + 1);
+        }
+
+        System.out.println("WESZŁO DO OCENIANIA KOMENTARZA");
+
+        dataAccessObject.save(comment);
     }
 }
