e492783e1549ff5fcd342a13beccbcc49ba300b9
Michał Pocheć (michpoc547) <michpoc547@student.polsl.pl>
2019-06-10 15:41:00 +0200

    Baza danych przełożona na nowy serwer

--- stronka/script.js
+++ stronka/script.js
@@ -1,10 +1,10 @@
 
   console.log("script.js")
-  $.get("http://185.24.216.103:25070/webpage/number/100/type/1",function(data, status){
+  $.get("http://77.55.217.170:25070/webpage/number/100/type/1",function(data, status){
 
         $("#myTable").mirandajs(data);
     });
-    $.get("http://185.24.216.103:25070/webpage/number/100/type/0",function(data, status){
+    $.get("http://77.55.217.170:25070/webpage/number/100/type/0",function(data, status){
 
         $("#myTableWorst").mirandajs(data);
     });
